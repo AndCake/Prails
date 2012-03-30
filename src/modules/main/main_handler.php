e4b8c21e4069be8655707280017e5211be12500b
AndCake <rk.klatcher@gmail.com>
2012-03-30 13:46:20 +0200

    fixed some problems with creation of additional directories (were not accessible)

--- src/modules/main/main_handler.php
+++ src/modules/main/main_handler.php
@@ -167,8 +167,8 @@ class MainHandler
 			echo base64_decode("R0lGODlhMwAxAIAAAAAAAP/// yH5BAAAAAAALAAAAAAzADEAAAK8jI+pBr0PowytzotTtbm/DTqQ6C3hGX ElcraA9jIr66ozVpM3nseUvYP1UEHF0FUUHkNJxhLZfEJNvol06tzwrgd LbXsFZYmSMPnHLB+zNJFbq15+SOf50+6rG7lKOjwV1ibGdhHYRVYVJ9Wn k2HWtLdIWMSH9lfyODZoZTb4xdnpxQSEF9oyOWIqp6gaI9pI1Qo7BijbF ZkoaAtEeiiLeKn72xM7vMZofJy8zJys2UxsCT3kO229LH1tXAAAOw==");
 			die();
 		}
-		@mkdir("cache");
-		@mkdir("static/images", 0644, true);
+		@mkdir("cache", 0755, true);
+		@mkdir("static/images", 0755, true);
 		@mkdir("log");
 		$arr_param["test"] = touch("cache/test");
 		list($main_version, $sub_version) = explode(".", phpversion("sqlite"));
